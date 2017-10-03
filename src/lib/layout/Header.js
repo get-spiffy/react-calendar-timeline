cf123e350e0139d1224044f4cecd75452df00ee7
malindamiller <malindamiller2016@gmail.com>
2017-10-03 11:34:53 -0400

    debugging.

--- src/lib/layout/Header.js
+++ src/lib/layout/Header.js
@@ -78,6 +78,7 @@ export default class Header extends Component {
   handleChange = (date) => {
     this.props.changeDate(date)
     this.toggleCalendar()
+    console.log("changing calendar")
   }
 
   // periodClick = (e) => {
@@ -87,41 +88,42 @@ export default class Header extends Component {
   //   // }
   // }
 
-  touchStart = (e) => {
-    if (e.touches.length === 1) {
-      this.setState({
-        touchTarget: e.target || e.touchTarget,
-        touchActive: true
-      })
-    }
-  }
-
-  touchEnd = (e) => {
-    if (!this.state.touchActive) {
-      return this.resetTouchState()
-    }
-
-    var changedTouches = e.changedTouches[0]
-    if (changedTouches) {
-      var elem = document.elementFromPoint(changedTouches.pageX, changedTouches.pageY)
-      if (elem !== this.state.touchTarget) {
-        return this.resetTouchState()
-      }
-    }
-
-    this.resetTouchState()
-    this.periodClick(e)
-  }
-
-  resetTouchState () {
-    this.setState({
-      touchTarget: null,
-      touchActive: false
-    })
-  }
+  // touchStart = (e) => {
+  //   if (e.touches.length === 1) {
+  //     this.setState({
+  //       touchTarget: e.target || e.touchTarget,
+  //       touchActive: true
+  //     })
+  //   }
+  // }
+  //
+  // touchEnd = (e) => {
+  //   if (!this.state.touchActive) {
+  //     return this.resetTouchState()
+  //   }
+  //
+  //   var changedTouches = e.changedTouches[0]
+  //   if (changedTouches) {
+  //     var elem = document.elementFromPoint(changedTouches.pageX, changedTouches.pageY)
+  //     if (elem !== this.state.touchTarget) {
+  //       return this.resetTouchState()
+  //     }
+  //   }
+  //
+  //   this.resetTouchState()
+  //   this.periodClick(e)
+  // }
+  //
+  // resetTouchState () {
+  //   this.setState({
+  //     touchTarget: null,
+  //     touchActive: false
+  //   })
+  // }
   toggleCalendar = (e) => {
     e && e.preventDefault()
     this.setState({isOpen: !this.state.isOpen})
+    console.log("toggle calendar")
   }
   render () {
     let timeLabels = []
@@ -216,7 +218,7 @@ export default class Header extends Component {
         headerStyle.width = `${canvasWidth}px`
       }
     }
-
+//onTouchStart={this.touchStart} onTouchEnd={this.touchEnd}
     return (
       <div ref='header' key='header' className='rct-header' onClick={this.toggleCalendar} style={headerStyle}>
       {
