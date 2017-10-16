86f392d79ef8018c67550efbd4ff006fdfaea51d
malindamiller <malindamiller2016@gmail.com>
2017-10-16 14:16:38 -0400

    disable select.

--- src/lib/items/Item.js
+++ src/lib/items/Item.js
@@ -416,10 +416,10 @@ export default class Item extends Component {
   };
 
   actualClick (e, clickType) {
-    console.log("target", e.target.nodeName)
-    if (this.props.canSelect && this.props.onSelect) {
-      this.props.onSelect(this.itemId, clickType, e)
-    }
+    if(!(e.target.nodeName == "path") && !(e.target.nodeName == "svg") && !(e.target.nodeName == "BUTTON")
+      if (this.props.canSelect && this.props.onSelect) {
+        this.props.onSelect(this.itemId, clickType, e)
+      }
   }
 
   renderContent () {
