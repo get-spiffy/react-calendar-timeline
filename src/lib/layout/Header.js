63280d847cd55a613c800eee6c3d7af25ad64d7c
malindamiller <malindamiller2016@gmail.com>
2018-05-10 11:46:58 -0400

    define variable

--- src/lib/layout/Header.js
+++ src/lib/layout/Header.js
@@ -135,7 +135,7 @@ export default class Header extends Component {
     let timeLabels = []
 const { canvasTimeStart, canvasTimeEnd, canvasWidth, lineHeight, visibleTimeStart, visibleTimeEnd, fixedHeader,
         minUnit, timeSteps, headerLabelGroupHeight, headerLabelHeight,
-        hasRightSidebar } = this.props
+        hasRightSidebar, headerPosition } = this.props
 
     const ratio = canvasWidth / (canvasTimeEnd - canvasTimeStart)
     const twoHeaders = minUnit !== 'year'
